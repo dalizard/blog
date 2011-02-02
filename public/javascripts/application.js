dc20b3a79eec6ac23825e7ebdb6067255c970377
Dimitar Haralanov <dimitar@forebits.com>
2011-02-02 15:24:11 +0200

    Bundle update

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+jQuery(document).ready(function () {
+	jQuery('.post').hover(
+		function () {
+			jQuery(this).find('footer').animate({
+				right: '-122px'
+			}, 200);
+		},
+		function () {
+			jQuery(this).find('footer').animate({
+				right: '-47px'
+			}, 200);
+		}
+	);
+});
