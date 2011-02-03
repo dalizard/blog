9e619f81d8b62c3010fec551b4b74dce6a137ffb
Dimitar Haralanov <dimitar@forebits.com>
2011-02-03 18:39:43 +0200

    Added hoverIntent plugin for homepage hover() effects

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,16 +2,28 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 jQuery(document).ready(function () {
-	jQuery('.post').hover(
-		function () {
-			jQuery(this).find('footer').animate({
-				right: '-122px'
-			}, 200);
-		},
-		function () {
-			jQuery(this).find('footer').animate({
-				right: '-47px'
-			}, 200);
+	jQuery(function() {
+		var config = {
+			sensitivity: 3,
+			interval: 200,
+			timeout: 200,
+			over: function() {
+				jQuery(this).find('a.read-more').animate({
+					right: '-122px'
+					}, 200);
+				jQuery(this).find('.share').animate({
+					bottom: '-3.2em'
+				}, 200);			
+			},
+			out: function() {
+				jQuery(this).find('a.read-more').animate({
+					right: '-47px'
+				}, 200);
+				jQuery(this).find('.share').animate({
+					bottom: '-1em'
+				}, 200);					
+			}
 		}
-	);
+		jQuery('.post').hoverIntent(config);
+	});
 });
