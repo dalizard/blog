14327a70e2c216cf0676f583bb49a76df29a88b2
Dimitar Haralanov <dimitar@forebits.com>
2011-02-25 16:47:11 +0200

    Database credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,16 @@
 set :application, "blog"
-set :repository,  "set your repository location here"
+set :repository,  "git://github.com/dalizard/blog.git"
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :deploy_to, "/www-data/blog"
+set :use_sudo, false
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+role :web, "calvin.forebits.com"                          # Your HTTP server, Apache/etc
+role :app, "calvin.forebits.com"                          # This may be the same as your `Web` server
+role :db,  "calvin.forebits.com", :primary => true # This is where Rails migrations will run
+role :db,  "calvin.forebits.com"
 
 # namespace :deploy do
 #   task :start do ; end
